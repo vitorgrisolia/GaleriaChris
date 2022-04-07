ea91b2a32fb4e34bee05cdd6bf0c3123b72d0559
VitorGrisolia <54046675+vitorgrisolia@users.noreply.github.com>
2022-04-06 21:57:19 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+     <h1>PaginaChris</h1>
+     <PaginaChris />
+  </div>
+</template>
+
+<script>
+import PaginaChris from "./main/paginaChris"
+export default {
+    components: { PaginaChris }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+}
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(10,37,156), rgb(253,50,88));
+}
+</style>
